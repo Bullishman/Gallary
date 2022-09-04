82ec62b8a24d038fa2c11438298f0bb7088b1a20
Bullishman <deapark123@gmail.com>
2022-09-04 22:11:10 +0900

    Delete

--- ecommerce/src/main/java/com/educative/ecommerce/repository/ItemRepository.java
+++
@@ -1,8 +0,0 @@
-package com.educative.ecommerce.repository;
-
-import com.educative.ecommerce.entity.Item;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ItemRepository extends JpaRepository<Item, Integer> {
-
-}
